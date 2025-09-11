c96069966c0c8ec27efb331f029e446236bfb769
htdxd <2679857218@qq.com>
2025-09-11 23:50:04 +0800

    重构了数据增强方法，增加resize

--- dataset/transform.py
+++ dataset/transform.py
@@ -5,6 +5,7 @@ transform = T.Compose([
     T.RandomPhotometricDistort(),
     T.RandomHorizontalFlip(p=0.5),
     T.ScaleJitter(target_size=(600, 600), scale_range=(0.8, 1.2)),
+    T.Resize((600, 600)),
     T.SanitizeBoundingBoxes(min_size=1.0),
     T.ToTensor(),         # 转为 Tensor[C, H, W]
     T.ConvertImageDtype(torch.float32),  # 归一化到 [0, 1]
